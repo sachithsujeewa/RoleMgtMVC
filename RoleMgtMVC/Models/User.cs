1828767024ff1791ffe3e08ed8ae75e1503de3fe
sachith sujeewa <sachithj@99x.io>
2021-03-13 14:17:51 +0530

    Add project files.

---
+++ RoleMgtMVC/Models/User.cs
@@ -0,0 +1,18 @@
+﻿namespace RoleMgtMVC.Models
+{
+    public enum UserRoles
+    {
+        External = 0,
+        InternalEmployee =1,
+        ContractEmployee =2,
+        LeadEmployee = 3,
+        AdminEmployee = 4
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public UserRoles UserRole { get; set; }
+    }
+}
